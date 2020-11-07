b75c5bcdf38c86269e226f1e513e99a5f01358a1
zidesk <zidesk@gmail.com>
2020-11-07 16:50:40 +0100

    finish module 5

--- budget/BudgetList.py
+++ budget/BudgetList.py
@@ -1,6 +1,7 @@
 from . import Expense
 import matplotlib.pyplot as plt
 
+
 class BudgetList:
     def __init__(self, budget):
         self.budget = budget
@@ -33,7 +34,6 @@ class BudgetList:
 
 
 def main():
-
     myBudgetList = BudgetList(1200)
     expenses = Expense.Expenses()
     expenses.read_expenses("data/spending_data.csv")
@@ -41,7 +41,7 @@ def main():
     for expense in expenses.list:
         myBudgetList.append(expense.amount)
 
-    print('The count of all expenses: '+str(len(myBudgetList)))
+    print('The count of all expenses: ' + str(len(myBudgetList)))
 
     for entry in myBudgetList:
         print(entry)
@@ -50,12 +50,10 @@ def main():
     labels = ['Expenses', 'Overages', 'Budget']
     values = [myBudgetList.sum_expenses, myBudgetList.sum_overages, myBudgetList.budget]
 
-    ax.bar(labels, values, color=['green','red','blue'])
+    ax.bar(labels, values, color=['green', 'red', 'blue'])
     ax.set_title("Your total expenses vs. total budget")
     plt.show()
 
 
-
 if __name__ == "__main__":
     main()
-
--- budget/ExpenseCategories.py
+++ budget/ExpenseCategories.py
@@ -1,10 +1,19 @@
 from . import Expense
 import matplotlib.pyplot as plt
 
+
 def main():
     expenses = Expense.Expenses()
     expenses.read_expenses('data/spending_data.csv')
     divided_for_loop = expenses.categorize_for_loop()
+    divided_set_comp = expenses.categorize_set_compreshension()
+
+    if divided_for_loop != divided_set_comp:
+        print("Sets are NOT equal by == test")
+
+    for a,b in zip(divided_for_loop, divided_set_comp):
+        if not (a.issubset(b) or b.issubset(a)):
+            print("Sets are NOT equal by subset test")
 
 
 if __name__ == "__main__":
